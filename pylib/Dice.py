c8250828549716eae88329d8d6381b7ed435017b
lowhrtz <lowhrtz@gmail.com>
2020-01-20 22:45:15 -0500

    Expanded the scope of the complex dice pattern

--- pylib/Dice.py
+++ pylib/Dice.py
@@ -15,7 +15,8 @@ def rollString(dice_string):
     single_dice_format = re.compile('^d?[0-9]+$')
     dice_format = re.compile('^[0-9]+[d][0-9]+$')
     # complex_format = re.compile('^\\(?[0-9]+[d][0-9]+(\\s*\\+[0-9]+\\s*)?\\)?(\\s*[x|×]\\s*[0-9]+)?$')
-    complex_format = re.compile(r'^\(?[0-9]+[d][0-9]+(\s*[+-]\s*[0-9]+\s*)?\)?(\s*[x|×]\s*[0-9]+)?$')
+    # complex_format = re.compile(r'^\(?[0-9]+[d][0-9]+(\s*[+-]\s*[0-9]+\s*)?\)?(\s*[x|×]\s*[0-9]+)?$')
+    complex_format = re.compile(r'^\(?(\d+\s*\+\s*)?[0-9]+[d][0-9]+(\s*[+-]\s*[0-9]+\s*)?\)?(\s*[x|×]\s*[0-9]+)?$')
 
     if single_dice_format.match(dice_string):
         sides = int(dice_string.replace('d', ''))
